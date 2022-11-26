54a398f92d6b489134f2c75f28f71a9ededc8b2e
AXON <axonasif@gmail.com>
2022-11-27 01:50:16 +0600

    fix: handle job control

--- conf.d/bashenv.fish
+++ conf.d/bashenv.fish
@@ -1,16 +1,25 @@
 #!/usr/bin/fish
 
-if status is-interactive
-    set bash_args '-cli'
-else
-    set bash_args '-cl'
+if status is-full-job-control;
+  set job_control_type full;
+else if status is-interactive-job-control;
+  set job_control_type interactive;
 end
 
+status job-control none;
+
 # TODO: Maybe we could also create a `declare` function on fish to avoid replacing it from bash
-bash $bash_args 'declare -px' \
+bash -lic 'declare -px' \
   | string replace --regex '^declare -x' export \
   | string replace --regex '^export (SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|_)=' ': export _=' \
   | source
+
+switch "$job_control_type";
+  case "interactive";
+    status job-control "$job_control_type";
+  case "full";
+    status job-control "$job_control_type";
+end
 
 ## Old method:
 # set -l var_split '<<;@@;>>'
